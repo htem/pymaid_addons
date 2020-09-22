728ea697c1f74bafd5d3531afc7351e29feccaa7
Jasper Maniates-Selvin <jtmaniatesselvin@g.harvard.edu>
2020-09-22 16:22:44 -0400

    Added setup.py for pip-installability

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymaid-addons",
+    version="0.0.1",
+    author="Jasper Phelps",
+    author_email="jtmaniatesselvin@g.harvard.edu",
+    description="Some functions built on top of pymaid",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/htem/pymaid_addons",
+    license='GNU GPL v3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    python_requires='>=3.6',
+)
